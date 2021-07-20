2ba72559dfd2b8c712b3aad0816ec153691104ae
Luis J. Montes <luisjavm3@gmail.com>
2021-07-20 10:38:14 -0500

    Register users

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+// Connect database
+connectDB();
+
+// Init middleware
+app.use(express.json());
+
 app.route('/').get((req, res) => res.send('API Running'));
+
+// Define routes
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
 
