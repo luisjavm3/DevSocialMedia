d559b8f42bbe4ee0769d547fb3b42334c2c7d2af
Luis J. Montes <luisjavm3@gmail.com>
2021-07-26 15:35:48 -0500

    Post like & unlike routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -91,6 +91,7 @@ router.route('/:id').delete(auth, async (req, res) => {
       return res.status(404).json({ msg: 'Post not found.' });
     }
 
+    // Check user
     if (post.user.toString() !== req.user.id) {
       return res.status(401).json({ msg: 'User not authorized.' });
     }
@@ -105,6 +106,63 @@ router.route('/:id').delete(auth, async (req, res) => {
       return res.status(500).json({ msg: 'Post not found.' });
     }
 
+    res.status(500).send('Server Error.');
+  }
+});
+
+// @route       PUT api/posts/like/:id
+// @desc        Like a post
+// @access      Private
+router.route('/like/:id').put(auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //Check if the post has already been liked.
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id).length >
+      0
+    ) {
+      return res.status(400).json({ msg: 'Post already liked.' });
+    }
+
+    post.likes.unshift({ user: req.user.id });
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error.');
+  }
+});
+
+// @route       PUT api/posts/unlike/:id
+// @desc        Like a post
+// @access      Private
+router.route('/unlike/:id').put(auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    //Check if the post has already been liked.
+    if (
+      post.likes.filter((like) => like.user.toString() === req.user.id)
+        .length === 0
+    ) {
+      return res.status(400).json({ msg: 'Post has not yet been liked.' });
+    }
+
+    // Get remove index
+    const removeIndex = post.likes
+      .map((like) => like.user.toString())
+      .indexOf(req.user.id);
+
+    post.likes.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (error) {
+    console.error(error.message);
     res.status(500).send('Server Error.');
   }
 });
