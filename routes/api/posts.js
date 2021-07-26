ff354ec15ed4556fc86c388a34ce4e3d19d4830a
Luis J. Montes <luisjavm3@gmail.com>
2021-07-26 14:44:37 -0500

    Find and delete post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -23,8 +23,90 @@ router
         });
       }
 
-      //   const user = await User.
+      try {
+        const user = await User.findById(req.user.id).select('-password');
+
+        const newPost = new Post({
+          user: req.user.id,
+          text: req.body.text,
+          name: user.name,
+          avatar: user.avatar,
+        });
+
+        const post = await newPost.save();
+
+        res.json(post);
+      } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error.');
+      }
     }
   );
+
+// @route       GET api/posts
+// @desc        Get all posts
+// @access      Private
+router.route('/').get(auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+
+    res.json(posts);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error.');
+  }
+});
+
+// @route       GET api/posts/:id
+// @desc        Get post by ID
+// @access      Private
+router.route('/:id').get(auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found.' });
+    }
+
+    res.json(post);
+  } catch (error) {
+    console.error(error.message);
+
+    if (error.kind === 'ObjectId') {
+      return res.status(500).json({ msg: 'Post not found.' });
+    }
+
+    res.status(500).send('Server Error.');
+  }
+});
+
+// @route       DELETE api/posts/:id
+// @desc        Delete a post
+// @access      Private
+router.route('/:id').delete(auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post not found.' });
+    }
+
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'User not authorized.' });
+    }
+
+    await post.remove();
+
+    res.json({ msg: 'Post removed.' });
+  } catch (error) {
+    console.error(error.message);
+
+    if (error.kind === 'ObjectId') {
+      return res.status(500).json({ msg: 'Post not found.' });
+    }
+
+    res.status(500).send('Server Error.');
+  }
+});
 
 module.exports = router;
