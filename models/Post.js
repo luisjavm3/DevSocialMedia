f10f139a0221c387fbf5236d0fc5e622d04dd3ca
Luis J. Montes <luisjavm3@gmail.com>
2021-07-26 10:28:58 -0500

    Create Post model

---
+++ models/Post.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'users',
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  avatar: {
+    type: String,
+  },
+  likes: [
+    {
+      user: mongoose.Schema.ObjectId,
+      ref: 'users',
+    },
+  ],
+  comments: [
+    {
+      user: {
+        user: mongoose.Schema.ObjectId,
+        ref: 'users',
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+      },
+      avatar: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = Post = mongoose.model('post', PostSchema);
