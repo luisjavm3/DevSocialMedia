bb39fa2ead0d2897e54b09e4e3d938da9361820e
Luis J. Montes <luisjavm3@gmail.com>
2021-07-28 10:34:42 -0500

    Client initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,32 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import store from './store';
+import './App.css';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+
+          <section className="container">
+            <Switch>
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" component={Login} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
