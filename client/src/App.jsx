132a8bbe9f3d16fe6dfe02cdd641ca2074e13833
Luis J. Montes <luisjavm3@gmail.com>
2021-08-03 15:41:16 -0500

    Complete AddExperience view

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,6 +11,7 @@ import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
+import AddExperience from './components/profile-forms/AddExperience';
 
 import setAuthToken from './utils/setAuthToken';
 import { loadUser } from './actions/auth';
@@ -49,6 +50,11 @@ function App() {
                 exact
                 path="/edit-profile"
                 component={EditProfile}
+              />
+              <PrivateRoute
+                exact
+                path="/add-experience"
+                component={AddExperience}
               />
             </Switch>
           </section>
