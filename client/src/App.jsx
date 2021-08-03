63f713eebf80ff1ce44fe7cc78ac252df0343429
Luis J. Montes <luisjavm3@gmail.com>
2021-08-03 16:20:54 -0500

    Last commit on 2021/08/3

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,7 @@ import Dashboard from './components/dashboard/Dashboard';
 import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
 import AddExperience from './components/profile-forms/AddExperience';
+import AddEducation from './components/profile-forms/AddEducation';
 
 import setAuthToken from './utils/setAuthToken';
 import { loadUser } from './actions/auth';
@@ -55,6 +56,11 @@ function App() {
                 exact
                 path="/add-experience"
                 component={AddExperience}
+              />
+              <PrivateRoute
+                exact
+                path="/add-education"
+                component={AddEducation}
               />
             </Switch>
           </section>
