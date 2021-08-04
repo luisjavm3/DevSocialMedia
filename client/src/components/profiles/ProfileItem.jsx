ddbfaafd44037aa305184a28237930077502b392
Luis J. Montes <luisjavm3@gmail.com>
2021-08-04 15:06:21 -0500

    Complete Profile Card

--- client/src/components/profiles/ProfileItem.jsx
+++ client/src/components/profiles/ProfileItem.jsx
@@ -24,6 +24,15 @@ const ProfileItem = ({
           View Profile
         </Link>
       </div>
+
+      <ul>
+        {skills.slice(0, 4).map((skill, index) => (
+          <li key={index} className="text-primary">
+            <i className="fas fa-check"></i>
+            {skill}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
